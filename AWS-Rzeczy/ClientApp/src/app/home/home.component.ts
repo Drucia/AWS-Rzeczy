ce227929d7ffe4930a6ebf2a5211ae6cc3b17f0f
tomek <rainbowvme@gmail.com>
2020-10-24 17:36:34 +0200

    Code format

--- AWS-Rzeczy/ClientApp/src/app/home/home.component.ts
+++ AWS-Rzeczy/ClientApp/src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent {
